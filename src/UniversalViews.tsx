e445fd2e443b972fab05d41a3c4e47d722e0c46b
evanbacon <bacon@expo.io>
2022-09-16 16:09:59 -0500

    + @bacons/react-views@1.0.6
    disable hover when hover style is not defined.

--- src/UniversalViews.tsx
+++ src/UniversalViews.tsx
@@ -1,58 +1,84 @@
-import React, { forwardRef, useMemo, useRef } from 'react';
-import { StyleSheet, View as UpstreamView, Text as UpstreamText, Image as UpstreamImage } from 'react-native';
+import React, { forwardRef, useMemo, useRef } from "react";
+import {
+    StyleSheet,
+    View as UpstreamView,
+    Text as UpstreamText,
+    Image as UpstreamImage,
+} from "react-native";
 
-import { filterStyles } from './filterStyles';
-import { ImageProps } from './Image';
-import { useMergeRefs } from './mergeRefs';
-import { TextProps } from './Text';
-import { useHover } from './useEvents';
-import { ViewProps } from './View';
-
+import { filterStyles } from "./filterStyles";
+import { ImageProps } from "./Image";
+import { useMergeRefs } from "./mergeRefs";
+import { TextProps } from "./Text";
+import { useHover } from "./useEvents";
+import { ViewProps } from "./View";
 
 function createPsuedoClassView<TView, TViewProps extends { style?: any }>(
     View: TView,
-    { style, hoverStyle, center, ...props }: TViewProps & { center?: boolean, hoverStyle?: any },
-    forwardedRef: React.Ref<TView>,
+    {
+        style,
+        center,
+        ...props
+    }: TViewProps & { center?: boolean; hoverStyle?: any },
+    forwardedRef: React.Ref<TView>
 ) {
-
     // Filter and apply `center` prop.
     const finalStyle = useMemo(() => {
-        const filteredStyle = filterStyles(style) ?? {}
+        const filteredStyle = filterStyles(style) ?? {};
         if (center) {
-            return [
-                styles.center,
-                filteredStyle,
-            ]
+            return [styles.center, filteredStyle];
         }
         return filteredStyle;
-    }, [style, center])
+    }, [style, center]);
 
-    const hostRef = useRef(null);
-    const setRef = useMergeRefs(forwardedRef, hostRef);
-    const hover = useHover(hostRef)
-
-    // if (View === Text && props.href && Platform.OS !== 'web') {
-    //     const originalOnPress = props.onPress?.bind(props);
-    //     props.onPress = (...args) => {
-    //         originalOnPress?.(...args);
-    //         Linking.openURL(props.href);
-    //     }
-    // }
+    const Klass = useMemo(() => {
+        if (props.hoverStyle) {
+            return forwardRef(
+                // @ts-expect-error
+                createHoverView.bind(this, View)
+            );
+        }
+        return View;
+    }, [props.hoverStyle]);
 
     return (
         // @ts-expect-error
-        <View ref={setRef} style={[finalStyle, hover && hoverStyle]} {...props} />
+        <Klass ref={forwardedRef} style={finalStyle} {...props} />
     );
 }
 
+function createHoverView(
+    View,
+    { hoverStyle, style, ...props }: ViewProps,
+    forwardedRef
+) {
+    const hostRef = useRef(null);
+    const setRef = useMergeRefs(forwardedRef, hostRef);
+    const hover = useHover(hostRef);
 
-export const View = forwardRef(createPsuedoClassView.bind(this, UpstreamView)) as React.ComponentType<ViewProps>;
-export const Text = forwardRef(createPsuedoClassView.bind(this, UpstreamText)) as React.ComponentType<TextProps>;
-export const Image = forwardRef(createPsuedoClassView.bind(this, UpstreamImage)) as React.ComponentType<ImageProps>;
+    const finalStyle = useMemo(() => {
+        if (Array.isArray(style)) {
+            return [...style, hover && hoverStyle];
+        }
+        return [style, hover && hoverStyle];
+    }, [style, hoverStyle, hover]);
+
+    return <View ref={setRef} style={finalStyle} {...props} />;
+}
+
+export const View = forwardRef(
+    createPsuedoClassView.bind(this, UpstreamView)
+) as React.ComponentType<ViewProps>;
+export const Text = forwardRef(
+    createPsuedoClassView.bind(this, UpstreamText)
+) as React.ComponentType<TextProps>;
+export const Image = forwardRef(
+    createPsuedoClassView.bind(this, UpstreamImage)
+) as React.ComponentType<ImageProps>;
 
 const styles = StyleSheet.create({
     center: {
         justifyContent: "center",
         alignItems: "center",
-    }
+    },
 });
