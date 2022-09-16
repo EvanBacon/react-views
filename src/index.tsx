38885999f05264d89d061cbb90a0ca44d00891b9
evanbacon <bacon@expo.io>
2022-09-16 15:32:13 -0500

    + @bacons/react-views@1.0.5
    export types

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,7 @@
 export { View, Image, Text } from './UniversalViews';
-export { Pressable } from './Pressable';
+export { Pressable, PressableProps, PressableStateCallbackType } from './Pressable';
 export { StyleSheet } from './StyleSheet';
+
+export { ViewStyle, ViewProps } from './View';
+export { TextStyle, TextProps } from './Text';
+export { ImageStyle, ImageProps } from './Image';
