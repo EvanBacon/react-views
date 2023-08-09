35c17852239fe6d6fb2882f86afb5745d2e21eae
evanbacon <bacon@expo.io>
2023-08-09 15:23:09 -0400

    Update View.tsx

--- src/View.tsx
+++ src/View.tsx
@@ -1,4 +1,4 @@
-import React, { ClassAttributes, ComponentProps, ComponentType } from 'react';
+import React, { ClassAttributes, ComponentProps, ComponentType } from "react";
 import {
   StyleProp,
   Text,
@@ -6,11 +6,13 @@ import {
   View as NativeView,
   AccessibilityRole,
   ViewStyle as NativeViewStyle,
-} from 'react-native';
+  StyleSheet,
+} from "react-native";
 
 // https://github.com/necolas/react-native-web/issues/832
 
-type NativeViewProps = ComponentProps<typeof NativeView> & ClassAttributes<typeof NativeView>;
+type NativeViewProps = ComponentProps<typeof NativeView> &
+  ClassAttributes<typeof NativeView>;
 
 /**
  * https://baconbrix.gitbook.io/react-native-web/primitives/view
@@ -29,7 +31,7 @@ export interface WebViewStyle {
   /** @platform web */
   animationName?: string | any[];
   /** @platform web */
-  animationIterationCount?: number | 'infinite';
+  animationIterationCount?: number | "infinite";
   /** @platform web */
   animationPlayState?: string;
   /** @platform web */
@@ -43,7 +45,7 @@ export interface WebViewStyle {
   /** @platform web */
   backgroundImage?: string;
   /** @platform web */
-  backgroundOrigin?: 'border-box' | 'content-box' | 'padding-box';
+  backgroundOrigin?: "border-box" | "content-box" | "padding-box";
   /** @platform web */
   backgroundPosition?: string;
   /** @platform web */
@@ -93,11 +95,11 @@ export interface WebViewStyle {
   /** @platform web */
   overflowY?: string;
   /** @platform web */
-  overscrollBehavior?: 'auto' | 'contain' | 'none';
+  overscrollBehavior?: "auto" | "contain" | "none";
   /** @platform web */
-  overscrollBehaviorX?: 'auto' | 'contain' | 'none';
+  overscrollBehaviorX?: "auto" | "contain" | "none";
   /** @platform web */
-  overscrollBehaviorY?: 'auto' | 'contain' | 'none';
+  overscrollBehaviorY?: "auto" | "contain" | "none";
   /** @platform web */
   perspective?: string;
   /** @platform web */
@@ -121,36 +123,37 @@ export interface WebViewStyle {
   /** @platform web */
   willChange?: string;
   /** @platform web */
-  position?: 'static' | 'relative' | 'absolute' | 'fixed' | 'sticky';
+  position?: "static" | "relative" | "absolute" | "fixed" | "sticky";
 }
 
-export type ViewStyle = Omit<NativeViewStyle, 'position'> & WebViewStyle;
+export type ViewStyle = Omit<NativeViewStyle, "position"> & WebViewStyle;
 
 export type WebViewProps = {
   style?: StyleProp<ViewStyle>;
 
   accessibilityRole?:
-  | 'list'
-  | 'listitem'
-  | 'complementary'
-  | 'contentinfo'
-  | 'region'
-  | 'navigation'
-  | 'main'
-  | 'article'
-  | 'banner'
-  | AccessibilityRole;
+    | "list"
+    | "listitem"
+    | "complementary"
+    | "contentinfo"
+    | "region"
+    | "navigation"
+    | "main"
+    | "article"
+    | "banner"
+    | AccessibilityRole;
 
   /** Float content to the center. `{ justifyContent: 'center', alignItems: 'center' }` */
   center?: boolean;
   hoverStyle?: ViewStyle;
 };
 
-export type ViewProps = WebViewProps & Omit<NativeViewProps, 'style' | 'accessibilityRole'>;
+export type ViewProps = WebViewProps &
+  Omit<NativeViewProps, "style" | "accessibilityRole">;
 
 let View = NativeView as ComponentType<ViewProps>;
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
   // Add better errors and warnings in development builds.
   View = function View(props: ViewProps) {
     const children = React.useMemo(() => {
@@ -159,21 +162,40 @@ if (process.env.NODE_ENV !== 'production') {
         if (child == null) {
           return;
         }
-        if (typeof child === 'string') {
+        if (typeof child === "string") {
           // Wrap text in a Text component.
-          console.warn(`Invalid raw text as a child of View: "${child}". Wrap it with a Text component or remove it.`);
-          children.push(<Text style={{ position: 'absolute', top: 0, left: 0, bottom: 0, right: 0, backgroundColor: 'firebrick', color: 'white', fontSize: 24 }}>Unwrapped text: "{<Text style={{ fontWeight: 'bold' }}>{child}</Text>}"</Text>)
+          let message = `Invalid raw text as a child of View: "${child}"${
+            child === "" ? ` [empty string]` : ""
+          }.`;
+          message +=
+            " Wrap the text contents with a Text element or remove it.";
+          console.warn(message);
+          children.push(
+            <Text style={[StyleSheet.absoluteFill, styles.error]}>
+              {'Unwrapped text: "'}
+              <Text style={{ fontWeight: "bold" }}>{child}</Text>
+              {'"'}
+            </Text>
+          );
           return;
-        } else if (typeof child?.type === 'string' && Platform.OS !== 'web') {
-          // Disallow react-dom elements on native.
-          throw new Error(`Using unsupported React DOM element "<${child.type} />" in React Native. Please remove this child from the View.`);
+        } else if (typeof child?.type === "string" && Platform.OS !== "web") {
+          // Disallow untransformed react-dom elements on native.
+          throw new Error(
+            `Using unsupported React DOM element: <${child.type} />`
+          );
         }
         children.push(child);
       });
       return children;
     }, [props.children]);
-    return <NativeView {...props} children={children} />
-  }
+    return <NativeView {...props} children={children} />;
+  };
+  const styles = StyleSheet.create({
+    error: {
+      backgroundColor: "firebrick",
+      color: "white",
+    },
+  });
 }
 
 export default View;
